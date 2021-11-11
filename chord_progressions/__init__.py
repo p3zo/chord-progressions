bc6aacff39d8fb40d40f3f9ff48afac5d768186f
p3zo <p3zo@users.noreply.github.com>
2021-11-11 22:20:49 +0000

    Bump version: 0.12.0 → 0.13.0

--- chord_progressions/__init__.py
+++ chord_progressions/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-__version__ = "0.12.0"  # updated by bumpversion, do not change
+__version__ = "0.13.0"  # updated by bumpversion, do not change
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
