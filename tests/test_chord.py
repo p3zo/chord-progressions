3887774d659ab4b9c8c8c9c572aee41f8223292e
p3zo <p3zo@users.noreply.github.com>
2022-09-13 11:53:30 -0700

    Use new chord.template attr

--- tests/test_chord.py
+++ tests/test_chord.py
@@ -1,4 +1,4 @@
-from chord_progressions.chord import get_template_from_notes
+from chord_progressions.chord import Chord
 
 EXPECTATIONS = [
     {"notes": ["C-1", "A4", "G9"], "template": [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0]}
@@ -7,4 +7,4 @@ EXPECTATIONS = [
 
 def test_get_template_from_notes():
     for ex in EXPECTATIONS:
-        assert get_template_from_notes(ex["notes"]) == ex["template"]
+        assert Chord(ex['notes']).template == ex["template"]
