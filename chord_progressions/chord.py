d27af5dfff01f20d3bd0300465710577733897f2
p3zo <p3zo@users.noreply.github.com>
2022-09-12 17:18:22 -0700

    Add template attribute to chord; remove chord_type from metrics

--- chord_progressions/chord.py
+++ chord_progressions/chord.py
@@ -49,6 +49,8 @@ class Chord:
         self.metrics = evaluate_notes_list(notes)
         self.template = get_template_from_notes(notes)
 
+        self.template = get_template_from_notes(chord)
+
     def json(self):
         return {
             "midi_nums": self.midi_nums,
